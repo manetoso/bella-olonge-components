3db17daae43107e764d3ce76d3f0132b118f1a42
manetoso <manetoso98@hotmail.com>
2021-08-07 15:31:47 -0500

    index.js added

---
+++ index.js
@@ -0,0 +1,27 @@
+import Container from './components/atoms/Layout/Container'
+import Spacer from './components/atoms/Layout/Spacer'
+
+import Buttom from './components/atoms/Button'
+import Header from './components/atoms/Header'
+import Icon from './components/atoms/Icon'
+import Logo from './components/atoms/Logo'
+import NavItem from './components/atoms/NavItem'
+import Paragraph from './components/atoms/Paragraph'
+import Picture from './components/atoms/Picture'
+import Separator from './components/atoms/Separator'
+
+import NavMenu from './components/molecules/NavMenu'
+
+export {
+  Container,
+  Spacer,
+  Buttom,
+  Header,
+  Icon,
+  Logo,
+  NavItem,
+  Paragraph,
+  Picture,
+  Separator,
+  NavMenu,
+}
